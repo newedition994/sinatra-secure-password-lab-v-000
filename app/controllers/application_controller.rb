5b188609c2ff26c48744aa39e91a4316d312c7d7
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-04 03:31:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
+      redirect '/login'
     end
   end
 
